c36c02cf408c443267b5b109cc27997359639805
Harshit <harshithkdigiverse@gmail.com>
2026-04-01 12:59:26 +0530

    Udpate in layout

--- src/Data/Layout.tsx
+++ src/Data/Layout.tsx
@@ -1,13 +1,13 @@
 import { PAGE_TITLE, ROUTES } from "../Constants";
 import type { NavItem } from "../Types";
-import { GridViewRounded, Home, Person } from "@mui/icons-material";
+import { BarChart, EditNote, GridViewRounded, Home, MiscellaneousServices, Person, Source } from "@mui/icons-material";
 
 export const NavItems: NavItem[] = [
   { icon: <GridViewRounded />, name: PAGE_TITLE.DASHBOARD, path: ROUTES.DASHBOARD },
   { icon: <Person />, name: PAGE_TITLE.PROFILE.BASE, path: ROUTES.PROFILE.BASE },
   { icon: <Home />, name: PAGE_TITLE.HERO_SECTION.BASE, path: ROUTES.HERO_SECTION.BASE },
-  { icon: <Home />, name: PAGE_TITLE.WORK_COUNT.BASE, path: ROUTES.WORK_COUNT.BASE },
-  { icon: <Home />, name: PAGE_TITLE.SERVICE.BASE, path: ROUTES.SERVICE.BASE },
-  { icon: <Home />, name: PAGE_TITLE.BLOG.BASE, path: ROUTES.BLOG.BASE },
-  { icon: <Home />, name: PAGE_TITLE.PORTFOLIO.BASE, path: ROUTES.PORTFOLIO.BASE },
+  { icon: <BarChart />, name: PAGE_TITLE.WORK_COUNT.BASE, path: ROUTES.WORK_COUNT.BASE },
+  { icon: <MiscellaneousServices />, name: PAGE_TITLE.SERVICE.BASE, path: ROUTES.SERVICE.BASE },
+  { icon: <EditNote />, name: PAGE_TITLE.BLOG.BASE, path: ROUTES.BLOG.BASE },
+  { icon: <Source />, name: PAGE_TITLE.PORTFOLIO.BASE, path: ROUTES.PORTFOLIO.BASE },
 ];
